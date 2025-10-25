377c55708c5076402236f4d986830bcdfbfdf382
akashshukla748972 <shuklaakash748972@gmail.com>
2025-10-25 15:18:46 +0530

    Added medical record controller logic

---
+++ server/controllers/medical.controller.js
@@ -0,0 +1,52 @@
+import medicalRecordModel from "../models/medicalRecord.model";
+import CustomeError from "../utils/CustomeError.js";
+
+// Add Medical Record
+export const addMedicalRecord = async (req, res, next) => {
+  try {
+    const { userId, recordType, description, fileUrl } = req.body;
+
+    const record = new medicalRecordModel({
+      userId,
+      recordType,
+      description,
+      fileUrl,
+    });
+    await record.save();
+
+    res
+      .status(201)
+      .json({ success: true, message: "Medical record added", data: record });
+  } catch (error) {
+    console.error(error);
+    return next(new CustomeError("Something went wrong", 500));
+  }
+};
+
+// Get User Medical Records
+export const getMedicalRecords = async (req, res, next) => {
+  try {
+    const records = await medicalRecordModel.find({
+      userId: req.params.userId,
+    });
+    res.status(200).json({ success: true, data: records });
+  } catch (error) {
+    console.error(error);
+    return next(new CustomeError("Something went wrong", 500));
+  }
+};
+
+// Delete Medical Record
+export const deleteMedicalRecord = async (req, res, next) => {
+  try {
+    const record = await medicalRecordModel.findByIdAndDelete(
+      req.params.recordId
+    );
+    if (!record) return next(new CustomeError("Record not found", 404));
+
+    res.status(200).json({ success: true, message: "Medical record deleted" });
+  } catch (error) {
+    console.error(error);
+    return next(new CustomeError("Something went wrong", 500));
+  }
+};
