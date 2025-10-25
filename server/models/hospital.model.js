efac4d5381143876bcb70f59f99fc6166c4e9e8d
akashshukla748972 <shuklaakash748972@gmail.com>
2025-10-25 14:21:49 +0530

    Added hospital model

---
+++ server/models/hospital.model.js
@@ -0,0 +1,15 @@
+import { Schema, model } from "mongoose";
+
+const hospitalSchema = new Schema(
+  {
+    userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    name: { type: String, required: true },
+    phone: { type: String, required: true },
+    email: { type: String },
+    address: { type: String },
+    isApproved: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
+const hospitalModel = model("hospital", hospitalSchema);
+export default hospitalModel;
