d96acad07c2a2e2c851de0f12e07b2c0c903fe9c
akashshukla748972 <shuklaakash748972@gmail.com>
2025-10-26 01:40:01 +0530

    Added redux setup

---
+++ web/src/store/store.js
@@ -0,0 +1,30 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { encryptTransform } from "redux-persist-transform-encrypt";
+
+import rootReducer from "./rootReducer";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  transforms: [
+    encryptTransform({
+      secretKey: "my-super-secret-key",
+      onError: function (error) {
+        console.log("Encryption error: ", error);
+      },
+    }),
+  ],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }),
+  devTools: import.meta.env.VITE_NODE_ENV === "development",
+});
+
+export const persistor = persistStore(store);
